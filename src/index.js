89420f5f75463984363b698bc80dbc8ddca95e0c
Bored_Panda <triden_et@mail.ru>
2018-09-23 11:56:15 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  count = 0;
+  for (var j =0; j < preferences.length; j++) {
+  if (j + 1 === preferences[preferences[preferences[j] - 1] - 1] && j + 1 !== preferences[j]) {
+  count++;
+          }
+      }
+  return count/3;
+  };
+  
